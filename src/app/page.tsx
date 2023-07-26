5c5ff1d633767ec1e5cefeadbddccd94a61f74f7
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2023-07-26 06:09:33 -0400

    feat: add pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,22 @@ export default function HomePage() {
   return (
     <main>
       <div className="absolute h-[100vw] w-[100vh] origin-top-left translate-y-[100vh] -rotate-90 overflow-x-hidden overflow-y-scroll">
-        <div className="flex w-[200vw] origin-top-left translate-x-[100vh] rotate-90">
+        <div className="flex w-[500vw] origin-top-left translate-x-[100vh] rotate-90">
+          <div className="slide h-screen w-screen">
+            <Section heading="Home"></Section>
+          </div>
           <div className="slide h-screen w-screen">
             <Section heading="About Us"></Section>
           </div>
-          <div className="slide h-screen w-screen bg-green-300"></div>
+          <div className="slide h-screen w-screen">
+            <Section heading="Products"></Section>
+          </div>
+          <div className="slide h-screen w-screen">
+            <Section heading="Blog"></Section>
+          </div>
+          <div className="slide h-screen w-screen">
+            <Section heading="Contact"></Section>
+          </div>
         </div>
       </div>
     </main>
